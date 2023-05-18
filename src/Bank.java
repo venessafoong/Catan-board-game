4465e0d5278d2ae20837c9f19477e6ca39058b3b
venessa.foong <venessa.foong@fdmgroup.com>
2023-05-18 20:03:53 +0800

    added building function in bank/player

--- src/Bank.java
+++ src/Bank.java
@@ -1,7 +1,29 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Bank
-{
-    ArrayList<Cards> cardInBank = new ArrayList<>();
+public class Bank {
+    private static Map<String, Integer> totalCardsInBank = Map.of("ore", 19, "wheat", 19, "brick", 19, "sheep", 19);
+    private static List<String> roadCost = Arrays.asList("brick", "wood");
+    private static List<String> settlementCost = Arrays.asList("brick", "wood", "sheep", "wheat");
+    private static List<String> cityCost = Arrays.asList("wheat", "wheat", "ore", "ore", "ore");
+    private static List<String> devCardCost = Arrays.asList("sheep", "wheat", "ore");
 
+    public static Map<String, Integer> getTotalCardsInBank() {
+        return totalCardsInBank;
+    }
+
+    public static List<String> getRoadCost() {
+        return roadCost;
+    }
+
+    public static List<String> getSettlementCost() {
+        return settlementCost;
+    }
+
+    public static List<String> getCityCost() {
+        return cityCost;
+    }
+
+    public static List<String> getDevCardCost() {
+        return devCardCost;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -5,10 +5,16 @@ public class Player
     private String name;
     private String colour;
     private ArrayList<String> cardsOnHand;
+    private int roads = 15;
+    private int settlements = 5;
+    private int cities = 4;
+    private ArrayList<String> initialResource;
 
-    public Player(String name, String colour) {
+    public Player(String name, String colour, ArrayList<String> initialResources)
+    {
         this.name = name;
         this.colour = colour;
+        this.initialResource = initialResources;
     }
 
     public void rollDice()
@@ -16,14 +22,46 @@ public class Player
         Dice.rollDice();
     }
 
-    public void gainResources(Cards ... resource)
+//    public void gainResources(Cards ... resource)
+//    {
+//        cardsOnHand.add();
+//    }
+//
+//    public void discardResources(Cards ... resource)
+//    {
+//        cardsOnHand.remove();
+//    }
+
+    public void buyRoad()
     {
-        //cardsOnHand.add();
+        if (cardsOnHand.containsAll(Bank.getRoadCost()))
+            cardsOnHand.removeAll(Bank.getRoadCost());
+        else
+            System.out.println("Not enough resources");
     }
 
-    public void discardResources(Cards ... resource)
+    public void buySettlement()
     {
-        //cardsOnHand.remove();
+        if (cardsOnHand.containsAll(Bank.getSettlementCost()))
+            cardsOnHand.removeAll(Bank.getSettlementCost());
+        else
+            System.out.println("Not enough resources");
+    }
+
+    public void buyCity()
+    {
+        if (cardsOnHand.containsAll(Bank.getCityCost()))
+            cardsOnHand.removeAll(Bank.getCityCost());
+        else
+            System.out.println("Not enough resources");
+    }
+
+    public void buyDevCard()
+    {
+        if (cardsOnHand.containsAll(Bank.getDevCardCost()))
+            cardsOnHand.removeAll(Bank.getDevCardCost());
+        else
+            System.out.println("Not enough resources");
     }
 
 }
